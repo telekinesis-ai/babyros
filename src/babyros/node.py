5f603cab1dde0e10c0579e7b300e04e8a4c9019c
Suman <suman7495@gmail.com>
2026-03-24 16:32:19 +0100

    Cleaned up SessionManager

--- src/babyros/node.py
+++ src/babyros/node.py
@@ -18,66 +18,6 @@ class SessionManager:
     _running = threading.Event()
     _config = None
 
-    def set_config_params(self,
-                connection_endpoints=["tcp/localhost:7447"],
-                multicast=False,
-                mode="peer",
-
-                router_timestamp=True,
-                peer_timestamp=True,
-                client_timestamp=True,
-
-                max_sessions=10000,
-                accept_pending=10000,
-
-                open_timeout=60000,
-                accept_timeout=60000,
-
-                connect_timeout_router=-1,
-                connect_timeout_peer=-1,
-                connect_timeout_client=0,
-
-                exit_on_failure_client=True,
-
-                retry_period_init_ms=1000,
-                retry_period_max_ms=4000,
-
-                gossip_to_router_only=True,
-
-                shared_memory=False,
-
-                queries_timeout=600000,
-                ):
-        
-        self._connection_endpoints = connection_endpoints
-        self._multicast = multicast
-        self._mode = mode
-
-        self._router_timestamp = router_timestamp
-        self._peer_timestamp = peer_timestamp
-        self._client_timestamp = client_timestamp
-
-        self._max_sessions = max_sessions
-        self._accept_pending = accept_pending
-
-        self._open_timeout = open_timeout
-        self._accept_timeout = accept_timeout
-
-        self._connect_timeout_router = connect_timeout_router
-        self._connect_timeout_peer = connect_timeout_peer
-        self._connect_timeout_client = connect_timeout_client
-
-        self._exit_on_failure_client = exit_on_failure_client
-
-        self._retry_period_init_ms = retry_period_init_ms
-        self._retry_period_max_ms = retry_period_max_ms
-
-        self._gossip_to_router_only = gossip_to_router_only
-
-        self._shared_memory = shared_memory
-
-        self._queries_timeout = queries_timeout
-
     @classmethod
     def get_session(cls):
         """
